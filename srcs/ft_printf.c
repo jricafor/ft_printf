a0245b6235315310dc5141ef01886358b6818ba3
jricafor <jricafor@student.42.fr>
2020-07-11 15:51:34 +0000

    update

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -1,6 +1,22 @@
 #include "../includes/printf.h"
+#include <stdarg.h>
 
-int ft_printf(const char *format, ...)
+int ft_printf(const char *src, ...)
 {
-	
+	int		i;
+	va_list my_list;
+
+	i = 0;
+	va_start(my_list, src);
+	while(src[i])
+	{
+		if(src[i - 1] != '%' || i != 0)
+			write(1, &src[i], 1);
+		i++;
+	}
+}
+
+int	main()
+{
+	ft_printf("Test \n nice\n");
 }
